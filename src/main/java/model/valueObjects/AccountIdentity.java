f180d35ca7486753797922d522dc206275d0dfea
ugoghub <ugovargst@gmail.com>
2026-05-26 14:05:42 -0300

    consertando o ultimo codigo que estava quebrado nos testes

--- src/main/java/model/valueObjects/AccountIdentity.java
+++ src/main/java/model/valueObjects/AccountIdentity.java
@@ -2,20 +2,26 @@ package model.valueObjects;
 
 import exception.InvalidAccountNumberException;
 import exception.InvalidBranchException;
-import exception.InvalidNullArgumentException;
 
 public record AccountIdentity(String branch, String accountNumber) {
 
     public AccountIdentity {
-        if(branch == null) throw new InvalidNullArgumentException("Agência não pode ser null");
-        if(accountNumber == null) throw new InvalidNullArgumentException("Número da conta não pode ser null");
+
+        if (branch == null) {
+            throw new InvalidBranchException("Agência inválida");
+        }
+
+        if (accountNumber == null) {
+            throw new InvalidAccountNumberException("Número da conta inválido");
+        }
 
         branch = branch.trim();
         accountNumber = accountNumber.trim();
 
-        if (branch.matches("\\d{2}")) {
+        if (!branch.matches("\\d{2}")) {
             throw new InvalidBranchException("Agência inválida");
         }
+
         if (!accountNumber.matches("\\d{6}-\\d")) {
             throw new InvalidAccountNumberException("Número da conta inválido");
         }
--- src/main/java/model/valueObjects/Email.java
+++ src/main/java/model/valueObjects/Email.java
@@ -7,7 +7,9 @@ import java.util.regex.Pattern;
 public record Email(String value) {
 
     private static final Pattern EMAIL_PATTERN =
-            Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\\\.[A-Za-z]{2,}$");
+            Pattern.compile(
+                    "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$"
+            );
 
     public Email {
 
